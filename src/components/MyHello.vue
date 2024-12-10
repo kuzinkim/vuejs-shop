fed31886b6c8222d2dc7c182d5aeb99875aee82b
Kim <kuzinhb2019@gmail.com>
2024-12-10 16:45:55 +0300

    инициализация проекта/верстка счетчика

---
+++ src/components/MyHello.vue
@@ -0,0 +1,38 @@
+<!-- Options API -->
+
+<!-- <script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+
+  methods: {
+    increment() {
+      this.count++
+    },
+  },
+}
+</script> -->
+
+<!-- Options API end -->
+
+<!-- Composition API -->
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0) // let count = 0
+
+const increment = () => {
+  count.value++
+}
+</script>
+
+<!-- Composition API end -->
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="increment">+</button>
+</template>
